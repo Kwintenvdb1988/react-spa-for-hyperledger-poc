e75ffb9e0bf124a6787447b5dd393bb9c616a02f
kwintenvandebroeck <kwintenvdb@gmail.com>
2019-12-14 19:45:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Contacts from './components/contacts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            contacts: [],
+            firstName: "",
+            lastName: ""
+        };
+    }
+    componentDidMount() {
+        fetch('http://localhost:8080/person')
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({contacts: data})
+            })
+            .catch(console.log)
+    }
+
+    handleFirstNameChange(e) {
+        this.setState({firstName: e.target.value});
+    }
+
+    handleLastNameChange(e) {
+        this.setState({lastName: e.target.value});
+    }
+
+    handleSendingData(firstName, lastName) {
+        console.log("posting data " + firstName + " " + lastName);
+        fetch('http://localhost:8080/person', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                "firstName": firstName,
+                "lastName": lastName
+            })
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text" placeholder="first name" value={this.firstName}
+                           onChange={(e) => this.handleFirstNameChange(e)}/>
+                    <input type="text" placeholder="last name" value={this.lastName}
+                           onChange={(e) => this.handleLastNameChange(e)}/>
+                </form>
+                <button type="button"
+                        onClick={() => this.handleSendingData(this.state.firstName, this.state.lastName)}>send data
+                </button>
+                <Contacts contacts={this.state.contacts}/>
+            </div>
+        );
+    }
 }
 
 export default App;
